86ebd095e847de3482eb04710aca425458d79827
hitesh bajaj <hitesshbajaj@gmail.com>
2019-02-05 00:03:40 +0530

    mina app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MinaApp::Application.config.secret_key_base = '3c46d8a3eeaf93202fd74577c438cf8cd3cd4653c668fc1ce2d5a038ff5cf924017875dae32b6468f93a795a02ab9179f1415231bee0558d2796d299c8cae4c4'
